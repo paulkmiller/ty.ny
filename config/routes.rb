dd97ce170a3475caecf96134ee4d344ef6423861
pmills88 <pmillerk88@gmail.com>
2014-09-11 09:18:46 -0400

    Added Readme

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :urls
   root 'urls#index'
-
-
-  # get "/auth/:provider/callback", to: "sessions#create"
   post "/create", to: "devise/sessions#create"
 end
 
